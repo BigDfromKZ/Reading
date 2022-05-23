36206aedcac22dabe2f6096b194f0497475fa841
BigDfromKZ <106057549+BigDfromKZ@users.noreply.github.com>
2022-05-23 16:16:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function play () {
+  var audio = new Audio ('https://www.youtube.com/watch?v=CelgqNnv0wU.mp3');
+  audio.play();
+};
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+
+  function myFunction() {
+    document.getElementById("demo").style.backgroundColor = "red";
+  };
